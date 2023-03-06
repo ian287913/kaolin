4603a49d9127b933c80cbc60f4aba9baacae3507
ian287913 <45512766+ian287913@users.noreply.github.com>
2023-03-06 15:56:29 +0800

    feat: train only texture with single rendered image

--- examples/tutorial/ian_utils.py
+++ examples/tutorial/ian_utils.py
@@ -1,9 +1,11 @@
+import os
 import torch
 import numpy as np
 from matplotlib import pyplot as plt
 from matplotlib.widgets import Slider, Button
 from pathlib import Path
 from PIL import Image
+import json
 
 ############################ Path ############################
 
@@ -30,3 +32,30 @@ def tensor2numpy(tensor:torch.Tensor) -> np.ndarray:
 def save_image(tensor_image:torch.Tensor, save_path, save_name):
     numpy_image = tensor2numpy(tensor_image[0])
     Image.fromarray(numpy_image).save(save_path / f"{save_name}_rgb.png")
+
+def import_png(path: Path):
+    if os.path.exists(path):
+        return torch.from_numpy(
+            np.array(Image.open(path))
+        )[:, :, :3].float() / 255.
+    else:
+        print(f"ERROR: file {path} does not exist.")
+        return None
+
+def load_rendered_png_and_camera_data(root_dir: Path, data_idx: int = 0):
+    # resolution follows 'rgb'
+    output = {}
+    output['rgb'] = import_png(os.path.join(root_dir, f'{data_idx}_rgb.png'))
+
+    with open(os.path.join(root_dir, f'{data_idx}_metadata.json'), 'r') as f:
+        fmetadata = json.load(f)
+        output['metadata'] = {
+            'cam_elev': fmetadata['cam_elev'],
+            'cam_azim': fmetadata['cam_azim'],
+            'cam_radius': fmetadata['cam_radius'],
+            'cam_look_at_height': fmetadata['cam_look_at_height'],
+            'cam_fovyangle': fmetadata['cam_fovyangle'],
+        }
+
+    print(f"loaded metadata = {output['metadata']}")
+    return output
