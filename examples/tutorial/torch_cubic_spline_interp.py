7bb41e8b745b8add3a702362d4970b9ad310c6e7
ian287913 <45512766+ian287913@users.noreply.github.com>
2023-03-10 15:47:43 +0800

    refactor: remove unused functions

--- examples/tutorial/torch_cubic_spline_interp.py
+++ examples/tutorial/torch_cubic_spline_interp.py
@@ -19,28 +19,6 @@ def h_poly(t):
     tt[i] = tt[i-1]*t
   return h_poly_helper(tt)
 
-def H_poly(t):
-  tt = [ None for _ in range(4) ]
-  tt[0] = t
-  for i in range(1, 4):
-    tt[i] = tt[i-1]*t*i/(i+1)
-  return h_poly_helper(tt)
-
-# ian: this is what i want!  the m could be assigned by ourself
-def interp_func(x, y):
-  "Returns integral of interpolating function"
-  if len(y)>1:
-    m = (y[1:] - y[:-1])/(x[1:] - x[:-1])
-    m = T.cat([m[[0]], (m[1:] + m[:-1])/2, m[[-1]]])
-  def f(xs):
-    if len(y)==1: # in the case of 1 point, treat as constant function
-      return y[0] + T.zeros_like(xs)
-    I = T.searchsorted(x[1:], xs)
-    dx = (x[I+1]-x[I])
-    hh = h_poly((xs-x[I])/dx)
-    return hh[0]*y[I] + hh[1]*m[I]*dx + hh[2]*y[I+1] + hh[3]*m[I+1]*dx
-  return f
-
 # m is tangent
 def interp_func_with_tangent(x, y, m, xs):
   "Returns integral of interpolating function"
@@ -50,33 +28,6 @@ def interp_func_with_tangent(x, y, m, xs):
   dx = (x[I+1]-x[I])
   hh = h_poly((xs-x[I])/dx)
   return hh[0]*y[I] + hh[1]*m[I]*dx + hh[2]*y[I+1] + hh[3]*m[I+1]*dx
-
-def interp(x, y, xs):
-  return interp_func(x,y)(xs)
-
-def integ_func(x, y):
-  "Returns interpolating function"
-  if len(y)>1:
-    m = (y[1:] - y[:-1])/(x[1:] - x[:-1])
-    m = T.cat([m[[0]], (m[1:] + m[:-1])/2, m[[-1]]])
-    Y = T.zeros_like(y)
-    Y[1:] = (x[1:]-x[:-1])*(
-        (y[:-1]+y[1:])/2 + (m[:-1] - m[1:])*(x[1:]-x[:-1])/12
-        )
-    Y = Y.cumsum(0)
-  def f(xs):
-    if len(y)==1:
-      return y[0]*(xs - x[0])
-    I = T.searchsorted(x[1:], xs)
-    dx = (x[I+1]-x[I])
-    hh = H_poly((xs-x[I])/dx)
-    return Y[I] + dx*(
-        hh[0]*y[I] + hh[1]*m[I]*dx + hh[2]*y[I+1] + hh[3]*m[I+1]*dx
-        )
-  return f
-
-def integ(x, y, xs):
-  return integ_func(x,y)(xs)
 
 def test_interpolation():
   # define keys
@@ -95,7 +46,7 @@ def test_interpolation():
 # See https://i.stack.imgur.com/zgA0s.png for resulting image
 if __name__ == "__main__":
   test_interpolation()
-
+  return
   import matplotlib.pylab as P # for plotting
   x = T.linspace(0, 6, 7)
   y = x.sin()
