bbb80ac3226c11d41ddaf94721fbbc2a7300c35a
ian287913 <45512766+ian287913@users.noreply.github.com>
2023-03-14 17:26:15 +0800

    rearrange the functions

--- examples/tutorial/ian_optimize_cubic_spline.py
+++ examples/tutorial/ian_optimize_cubic_spline.py
@@ -15,136 +15,13 @@ import kaolin as kal
 import ian_torch_cubic_spline_interp
 
 # Hyperparameters
-num_epoch = 200
 y_weight = 100
-
 y_lr = 5
 t_lr = 5
 scheduler_step_size = 20
 scheduler_gamma = 0.5
 
-################################## Prepare Parameters ##################################
-
-key_size = 3
-key_xs = None
-key_ys = None
-key_ts = None
-
-
-def prepare_loss():
-    global key_xs
-    global key_ys
-    global key_ts
-
-    key_xs = torch.linspace(0, 1, key_size, dtype=torch.float, device='cuda')
-
-    key_ys = torch.zeros((key_size), dtype=torch.float, device='cuda',
-                                requires_grad=True)
-    key_ts = torch.zeros((key_size), dtype=torch.float, device='cuda',
-                                requires_grad=True)
-    
-key_ys_optim = None
-key_ts_optim = None
-key_ys_scheduler = None
-key_ts_scheduler = None
-
-def setup_optimizer():
-    global key_ys_optim
-    global key_ts_optim
-    global key_ys_scheduler
-    global key_ts_scheduler
-    key_ys_optim  = torch.optim.Adam(params=[key_ys], lr = y_lr)
-    key_ts_optim  = torch.optim.Adam(params=[key_ts], lr = t_lr)
-
-    key_ys_scheduler = torch.optim.lr_scheduler.StepLR(
-        key_ys_optim,
-        step_size=scheduler_step_size,
-        gamma=scheduler_gamma)
-    key_ts_scheduler = torch.optim.lr_scheduler.StepLR(
-        key_ts_optim,
-        step_size=scheduler_step_size,
-        gamma=scheduler_gamma)
-
-def calculate_ground_truth(sample_xs):
-    gt_key_size = 3
-    gt_key_xs = torch.as_tensor(np.array([0, 0.5, 1]), dtype=torch.float, device='cuda')
-    gt_key_ys = torch.as_tensor(np.array([0, -1, 2]), dtype=torch.float, device='cuda')
-    gt_key_ts = torch.as_tensor(np.array([10, -2, -1]), dtype=torch.float, device='cuda')
-
-    return ian_torch_cubic_spline_interp.interp_func_with_tangent(gt_key_xs, gt_key_ys, gt_key_ts, sample_xs)
-
-################################## Training ##################################
-
-sample_size = 100
-sample_xs = None
-
-def train():
-    global sample_xs
-    sample_xs = torch.linspace(0, 1, sample_size, dtype=torch.float, device='cuda')
-
-    gt_ys = calculate_ground_truth(sample_xs)
-
-    for epoch in range(num_epoch):
-        if (epoch % 10 == 0):
-            visualize_results(sample_xs, epoch)
-
-        loss = train_iter(epoch, sample_xs, gt_ys)
-        
-        # step scheduler
-        key_ys_scheduler.step()
-        key_ts_scheduler.step()
-        print(f"Epoch {epoch} - loss: {float(loss)}")
-
-    visualize_results(sample_xs, num_epoch)
-
-def train_by_class():
-    lod = 3
-    spline_optimizer = SplineOptimizer(
-        lod, 
-        y_weight=y_weight, 
-        y_lr=y_lr, 
-        t_lr=t_lr, 
-        scheduler_step_size=scheduler_step_size, 
-        scheduler_gamma=scheduler_gamma)
-    
-    sample_xs = torch.linspace(0, 1, sample_size, dtype=torch.float, device='cuda')
-
-    gt_ys = calculate_ground_truth(sample_xs)
-
-    for epoch in range(num_epoch):
-        if (epoch % 10 == 0):
-            visualize_results_by_class(spline_optimizer, sample_xs, epoch)
-
-        spline_optimizer.zero_grad()
-        ys = spline_optimizer.calculate_ys(sample_xs)
-        ### Compute Losses ###
-        y_loss = torch.mean(torch.abs(gt_ys - ys))
-        loss = (y_loss * y_weight)
-        ### Update the mesh ###
-        loss.backward()
-        spline_optimizer.step()
-        print(f"Epoch {epoch} - loss: {float(loss)}")
-
-    visualize_results_by_class(spline_optimizer, sample_xs, num_epoch)
-
-def train_iter(epoch: int, sample_xs, gt_ys):
-    # reset gradient
-    key_ys_optim.zero_grad()
-    key_ts_optim.zero_grad()
-
-    # calculate sampled_Ys
-    ys = ian_torch_cubic_spline_interp.interp_func_with_tangent(key_xs, key_ys, key_ts, sample_xs)
-
-    ### Compute Losses ###
-    y_loss = torch.mean(torch.abs(gt_ys - ys))
-    loss = (y_loss * y_weight)
-
-    ### Update the mesh ###
-    loss.backward()
-    key_ys_optim.step()
-    key_ts_optim.step()
-
-    return loss
+################################## Class Definition ##################################
 
 class SplineOptimizer:
     def __init__(self, lod = 3, y_weight = 10, y_lr = 5e-1, t_lr = 5e-1, scheduler_step_size = 20, scheduler_gamma = 0.5):
@@ -193,8 +70,51 @@ class SplineOptimizer:
         self.key_ys_scheduler.step()
         self.key_ts_scheduler.step()
 
+################################## Utils ##################################
 
-def visualize_results_by_class(spline_optimizer:SplineOptimizer, sample_xs, epoch = 0):
+def calculate_ground_truth(sample_xs):
+    gt_key_size = 3
+    gt_key_xs = torch.as_tensor(np.array([0, 0.5, 1]), dtype=torch.float, device='cuda')
+    gt_key_ys = torch.as_tensor(np.array([0, -1, 2]), dtype=torch.float, device='cuda')
+    gt_key_ts = torch.as_tensor(np.array([10, -2, -1]), dtype=torch.float, device='cuda')
+    return ian_torch_cubic_spline_interp.interp_func_with_tangent(gt_key_xs, gt_key_ys, gt_key_ts, sample_xs)
+
+def train_spline():
+    # parameters
+    num_epoch = 100
+    visualize_epoch_interval = 10
+    sample_size = 100
+    lod = 3
+
+    spline_optimizer = SplineOptimizer(
+        lod, 
+        y_weight=y_weight, 
+        y_lr=y_lr, 
+        t_lr=t_lr, 
+        scheduler_step_size=scheduler_step_size, 
+        scheduler_gamma=scheduler_gamma)
+    
+    sample_xs = torch.linspace(0, 1, sample_size, dtype=torch.float, device='cuda')
+
+    gt_ys = calculate_ground_truth(sample_xs)
+
+    for epoch in range(num_epoch):
+        if (epoch % visualize_epoch_interval == 0):
+            visualize_results(spline_optimizer, sample_xs, epoch)
+
+        spline_optimizer.zero_grad()
+        ys = spline_optimizer.calculate_ys(sample_xs)
+        ### Compute Losses ###
+        y_loss = torch.mean(torch.abs(gt_ys - ys))
+        loss = (y_loss * y_weight)
+        ### Update the parameters ###
+        loss.backward()
+        spline_optimizer.step()
+        print(f"Epoch {epoch} - loss: {float(loss)}")
+
+    visualize_results(spline_optimizer, sample_xs, num_epoch)
+
+def visualize_results(spline_optimizer:SplineOptimizer, sample_xs, epoch = 0):
     with torch.no_grad():
         ys = spline_optimizer.calculate_ys(sample_xs)
     gt_ys = calculate_ground_truth(sample_xs)
@@ -209,26 +129,8 @@ def visualize_results_by_class(spline_optimizer:SplineOptimizer, sample_xs, epoc
     
     ##pylab.close()
 
-def visualize_results(sample_xs, epoch = 0):
-    with torch.no_grad():
-        ys = ian_torch_cubic_spline_interp.interp_func_with_tangent(key_xs, key_ys, key_ts, sample_xs)
-
-    gt_ys = calculate_ground_truth(sample_xs)
-
-    pylab.scatter(sample_xs.cpu(), gt_ys.cpu(), label='Ground Truth', color='blue')
-    pylab.plot(sample_xs.cpu(), ys.cpu(), label='Interpolated Curve', color='red')
-    pylab.scatter(key_xs.detach().cpu(), key_ys.detach().cpu(), color='red')
-    ##pylab.scatter(key_xs.detach().cpu(), key_ys.detach().cpu(), label='Trained Keys', color='red')
-    pylab.legend()
-    pylab.show()
-    
     ##pylab.savefig(f"./optimization record/{epoch}.png")
     ##pylab.close()
 
-
 if __name__ == "__main__":
-    train_by_class()
-
-    # prepare_loss()
-    # setup_optimizer()
-    # train()
+    train_spline()
