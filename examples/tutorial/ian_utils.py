06b02681323cfc1966dcdbdb4da6f4fcdbb7a32f
ian287913 <45512766+ian287913@users.noreply.github.com>
2023-03-15 15:45:37 +0800

    feat: create linspace function for pytorch tensor

--- examples/tutorial/ian_utils.py
+++ examples/tutorial/ian_utils.py
@@ -75,6 +75,28 @@ def get_camera_projection(fovyangle):
     camera_proj = kal.render.camera.generate_perspective_projection(fovyangle).to('cuda')
     return camera_proj
 
+############################ torch linspace ############################
+
+# from https://github.com/pytorch/pytorch/issues/61292
+def torch_linspace(start: torch.Tensor, stop: torch.Tensor, num: int):
+    """
+    Creates a tensor of shape [num, *start.shape] whose values are evenly spaced from start to end, inclusive.
+    Replicates but the multi-dimensional bahaviour of numpy.linspace in PyTorch.
+    """
+    # create a tensor of 'num' steps from 0 to 1
+    steps = torch.arange(num, dtype=torch.float32, device=start.device) / (num - 1)
+    
+    # reshape the 'steps' tensor to [-1, *([1]*start.ndim)] to allow for broadcastings
+    # - using 'steps.reshape([-1, *([1]*start.ndim)])' would be nice here but torchscript
+    #   "cannot statically infer the expected size of a list in this contex", hence the code below
+    for i in range(start.ndim):
+        steps = steps.unsqueeze(-1)
+    
+    # the output starts at 'start' and increments until 'stop' in each dimension
+    out = start[None] + steps*(stop - start)[None]
+    
+    return out
+
 ############################ Save Image ############################
 
 def tensor2numpy(tensor:torch.Tensor) -> np.ndarray:
