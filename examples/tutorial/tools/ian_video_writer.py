ef70b438ccf63f0043e4bb66bf6b0a552267b67f
ian287913 <45512766+ian287913@users.noreply.github.com>
2023-03-30 18:17:42 +0800

    feat: try to make video with images

---
+++ examples/tutorial/tools/ian_video_writer.py
@@ -0,0 +1,21 @@
+import cv2
+import os
+
+def write_video():
+    image_folder = 'images'
+    video_name = 'video.avi'
+
+    images = [img for img in os.listdir(image_folder) if img.endswith(".png")]
+    frame = cv2.imread(os.path.join(image_folder, images[0]))
+    height, width, layers = frame.shape
+
+    video = cv2.VideoWriter(video_name, 0, 1, (width,height))
+
+    for image in images:
+        video.write(cv2.imread(os.path.join(image_folder, image)))
+
+    cv2.destroyAllWindows()
+    video.release()
+
+if __name__ == "__main__":
+    write_video()
